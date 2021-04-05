b227372ad3b7693c157be932144360b21f931b7d
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-05 13:59:53 +0100

    working on the sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function Sidebar() {
+  return (
+    <Container>
+      <h2>sidebar</h2>
+    </Container>
+  );
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
