86306024ea91a4c55ba5dfb453c85c86e690e541
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-07 18:39:18 +0100

    Update ChatScreen.js

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
 import { auth } from "../firebase";
+import getRecipientEmail from "../utils/getRecipientEmail";
 
 function ChatScreen({ chat, messages }) {
   const [user] = useAuthState(auth);
